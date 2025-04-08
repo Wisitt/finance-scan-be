68c1ba9d84352936dcf975b22d0d4947442594c6
wisit <wisitmoondet@gmail.com>
2025-04-09 01:46:36 +0700

    d

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,14 @@ FROM node:18 AS builder
 WORKDIR /app
 COPY . .
 RUN npm install
+RUN npx prisma generate
 RUN npm run build
 
 FROM node:18-slim
 WORKDIR /app
 COPY --from=builder /app/dist ./dist
+COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/package*.json ./
-RUN npm install --omit=dev
+
 EXPOSE 8080
 CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT || 8080;
-  const frontendUrl = process.env.FRONTEND_URL || `http://localhost:3000`;
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
 
   app.enableCors({
     origin: frontendUrl,
