0419f8829244ab3334a82f30dcfb0d7c85333162
wisit <wisitmoondet@gmail.com>
2025-04-08 20:46:25 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
 
-  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
+  const port = process.env.PORT || 8080;
+  const frontendUrl = process.env.FRONTEND_URL || `http://localhost:3000`;
+
   app.enableCors({
     origin: frontendUrl,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -18,8 +19,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   Logger.log(`CORS enabled for origin: ${frontendUrl}`, 'Bootstrap');
-  Logger.log('Global ValidationPipe enabled', 'Bootstrap');
+  Logger.log(`App listening on port ${port}`, 'Bootstrap');
 
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(port);
 }
 bootstrap();
